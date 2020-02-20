8cb3dd6fc069c52087f708a12f220a15784353fd
YevhenBondarenko <ybondarenko@thingsboard.io>
2020-02-20 18:39:32 +0200

    created rest client example for PE

--- src/main/java/org/thingsboard/example/RestClientExample.java
+++ src/main/java/org/thingsboard/example/RestClientExample.java
@@ -1,9 +1,18 @@
 package org.thingsboard.example;
 
-import org.thingsboard.rest.client.RestClient;
-import org.thingsboard.server.common.data.Device;
+import org.thingsboard.client.tools.RestClient;
+import org.thingsboard.server.common.data.Customer;
+import org.thingsboard.server.common.data.EntityType;
 import org.thingsboard.server.common.data.asset.Asset;
-import org.thingsboard.server.common.data.relation.EntityRelation;
+import org.thingsboard.server.common.data.group.EntityGroup;
+import org.thingsboard.server.common.data.group.EntityGroupInfo;
+import org.thingsboard.server.common.data.permission.GroupPermission;
+import org.thingsboard.server.common.data.permission.Operation;
+import org.thingsboard.server.common.data.role.Role;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class RestClientExample {
     public static void main(String[] args) {
@@ -18,23 +27,44 @@ public class RestClientExample {
         RestClient client = new RestClient(url);
         client.login(username, password);
 
-        // creating and saving asset
+        // creating customer
+        Customer customer = new Customer();
+        customer.setTitle("Customer_1");
+        customer = client.saveCustomer(customer);
+
+        // getting default customer group "Customer Users"
+        List<EntityGroupInfo> customerGroups = client.getEntityGroupsByOwnerAndType(customer.getId(), EntityType.USER);
+        EntityGroupInfo customerUsersGroup = customerGroups
+                .stream()
+                .filter(entityGroupInfo -> entityGroupInfo.getName().equals("Customer Users"))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("Group not found"));
+
+        // creating asset
         Asset asset = new Asset();
         asset.setName("building_1");
         asset.setType("building");
         asset = client.saveAsset(asset);
 
-        // creating and saving device
-        Device device = new Device();
-        device.setName("thermometer_1");
-        device.setType("thermometer");
-        device = client.saveDevice(device);
+        // creating entity group for assets
+        EntityGroup entityGroup = new EntityGroup();
+        entityGroup.setName("buildings");
+        entityGroup.setType(EntityType.ASSET);
+        entityGroup = client.saveEntityGroup(entityGroup);
 
-        // creating relations from device to asset
-        EntityRelation relation = new EntityRelation();
-        relation.setFrom(device.getId());
-        relation.setTo(asset.getId());
-        relation.setType("default_relation");
-        client.saveRelation(relation);
+        // adding asset to entity group
+        client.addEntitiesToEntityGroup(entityGroup.getId(), Collections.singletonList(asset.getId()));
+
+        // creating role for user group
+        Role role = client.createGroupRole("read_only", Arrays.asList(Operation.READ_ATTRIBUTES, Operation.READ_TELEMETRY));
+
+        // creating permissions for user group with role by entity group with assets
+        GroupPermission groupPermission = new GroupPermission();
+        groupPermission.setEntityGroupId(entityGroup.getId());
+        groupPermission.setRoleId(role.getId());
+        groupPermission.setUserGroupId(customerUsersGroup.getId());
+        groupPermission.setEntityGroupId(entityGroup.getId());
+        groupPermission.setEntityGroupType(EntityType.ASSET);
+        groupPermission = client.saveGroupPermission(groupPermission);
     }
 }
